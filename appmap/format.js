f33de283109122f6717eb646c99e46c4cc830fdb
Laurent Christophe <lachrist@vub.ac.be>
2021-06-14 18:23:51 +0200

    reduced appmap and sorted event by id

--- appmap/format.js
+++ appmap/format.js
@@ -2,7 +2,7 @@ const name = process.argv[2];
 
 const data = JSON.parse(require("fs").readFileSync(name, 'utf8'));
 
-data.events.sort((event1, event2) => event1.id - event2.id);
+// data.events.sort((event1, event2) => event1.id - event2.id);
 
 process.stdout.write(
   JSON.stringify(data, null, 2),
